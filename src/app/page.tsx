da68b9c8114225c0ea9bb732e75a1b33f96b7e01
doinel1a <doinel1atanasiu@gmail.com>
2024-04-16 19:00:00 +0200

    refactor(home-page): add authentication buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,46 @@
 import React from 'react';
 
+import Link from 'next/link';
+
 import Counter from '@/components/counter';
 import GithubCorner from '@/components/github-corner';
+import { Button } from '@/components/ui/button';
+import { getServerAuthSession } from '@/server/auth';
 import { database } from '@/server/database';
 
 export default async function Home() {
-  const users = await database.user.findMany();
+  const session = await getServerAuthSession();
+  const testUsers = await database.testUser.findMany();
 
   return (
-    <main className='flex h-full w-full items-center justify-center gap-x-5'>
+    <main className='flex h-full w-full flex-col items-center justify-center '>
       <GithubCorner
         title='Get started on GitHub'
         url='https://github.com/doinel1a/next-ts-shadcn-ui'
       />
-      <Counter />
 
-      <ul className='flex w-72 flex-col gap-y-2 rounded-md border-2 border-border p-2'>
-        <h2 className='text-lg'>Users list</h2>
-        {users?.map((user) => (
-          <li key={user.id} className='rounded-md border border-border p-2'>
-            {user.name}
-          </li>
-        ))}
-      </ul>
+      {session && <h1 className='mb-5 text-3xl'>Welcome back, {session.user.name}</h1>}
+
+      <div className='flex gap-x-5'>
+        <Counter />
+
+        {testUsers && testUsers.length > 0 && (
+          <ul className='flex w-72 flex-col gap-y-2 rounded-md border-2 border-border p-2'>
+            <h2 className='text-lg'>Users list</h2>
+            {testUsers?.map((user) => (
+              <li key={user.id} className='rounded-md border border-border p-2'>
+                {user.name}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+
+      <Button variant={session ? 'destructive' : 'default'} className='mt-5' asChild>
+        <Link href={session ? '/api/auth/signout' : '/api/auth/signin'}>
+          {session ? 'Sign out' : 'Sign in'}
+        </Link>
+      </Button>
     </main>
   );
 }
