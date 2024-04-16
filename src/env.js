42e4b6cd0c6aad924a73fe9b1e3f3065df1cea59
doinel1a <doinel1atanasiu@gmail.com>
2024-04-16 18:58:47 +0200

    feat: install & configure next-auth using prisma adapter

--- src/env.js
+++ src/env.js
@@ -8,13 +8,26 @@ export const env = createEnv({
     // NEXT_PUBLIC_CLIENT_VAR: z.string(),
   },
   server: {
-    NODE_ENV: z.enum(['development', 'test', 'production']).default('development')
+    NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
+    DATABASE_URL: z.string().url(),
+    NEXTAUTH_URL: z.preprocess(
+      (str) => process.env.VERCEL_URL ?? str,
+      process.env.VERCEL ? z.string() : z.string().url()
+    ),
+    NEXTAUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string() : z.string().optional(),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string()
   },
   runtimeEnv: {
     // Client
     // NEXT_PUBLIC_CLIENT_VAR: process.env.NEXT_PUBLIC_CLIENT_VAR,
     // Server
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
---
+++ src/server/auth.ts
@@ -0,0 +1,59 @@
+import type { DefaultSession, NextAuthOptions } from 'next-auth';
+
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import { getServerSession } from 'next-auth';
+import { type Adapter } from 'next-auth/adapters';
+import GoogleProvider from 'next-auth/providers/google';
+
+import { env } from '@/env';
+
+import { database } from './database';
+
+/**
+ * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
+ * object and keep type safety.
+ *
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+declare module 'next-auth' {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession['user'];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    session: ({ session, user }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: user.id
+      }
+    })
+  },
+  adapter: PrismaAdapter(database) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code'
+        }
+      }
+    })
+  ]
+};
+
+export const getServerAuthSession = () => getServerSession(authOptions);
